5c3e8f8e191f9e72e61505644984859e492012c9
DesnLee <jiakun.ui@gmail.com>
2022-02-11 11:00:27 +0800

    修复 Dialog 滚动 bug

--- src/components/openDialog.ts
+++ src/components/openDialog.ts
@@ -2,79 +2,69 @@ import { createApp, h } from 'vue';
 import Dialog from './Dialog.vue';
 
 interface Options {
-  title?: Function;
-  content?: Function;
-  ok?: Function;
-  cancel?: Function;
-  closeOnOverlay?: Boolean;
+	title?: Function;
+	content?: Function;
+	ok?: Function;
+	cancel?: Function;
+	closeOnOverlay?: Boolean;
 }
 
 export const openDialog = (options: Options) => {
-  let { title, content, ok, cancel, closeOnOverlay } = options;
-  title = title
-          ? title
-          : () => {
-      return 'Dialog 默认标题';
-    };
-  content = content
-            ? content
-            : () => {
-      return 'Dialog 默认内容';
-    };
-  ok = ok
-       ? ok
-       : () => {
-      console.log('Dialog - 默认ok');
-    };
-  cancel = cancel
-           ? cancel
-           : () => {
-      console.log('Dialog - 默认cancel');
-    };
+	let { title, content, ok, cancel, closeOnOverlay } = options;
+	title = title
+		? title
+		: () => {
+				return 'Dialog 默认标题';
+		  };
+	content = content
+		? content
+		: () => {
+				return 'Dialog 默认内容';
+		  };
+	ok = ok
+		? ok
+		: () => {
+				console.log('Dialog - 默认ok');
+		  };
+	cancel = cancel
+		? cancel
+		: () => {
+				console.log('Dialog - 默认cancel');
+		  };
 
-  const container = document.createElement('div');
-  document.body.appendChild(container);
+	const container = document.createElement('div');
+	const moveEvent = e => {e.preventDefault();};
 
-  const close = () => {
-    app.unmount(container);
-    container.remove();
-  };
+	document.body.appendChild(container);
 
-  const moveEvent = e => {
-    e.preventDefault();
-  };
+	const close = () => {
+		app.unmount(container);
+		document.body.style.overflow = ''; //出现滚动条
+		document.removeEventListener('touchmove', moveEvent, false);
+		container.remove();
+	};
 
-  const app = createApp({
-    render() {
-      return h(
-        Dialog,
-        {
-          closeOnOverlay,
-          visible: true,
-          'onUpdate:visible': (vis: Boolean) => {
-            if (!vis) {
-              console.log('关');
-              document.body.style.overflow = ''; //出现滚动条
-              document.removeEventListener('touchmove', moveEvent, false);
-              close();
-            }
-          },
-          cancel,
-          ok,
-          nativeOn: {
-            'touchmove': e => {
-              console.log('开');
-              document.body.style.overflow = 'hidden'; //出现滚动条
-              document.addEventListener('touchmove', moveEvent, false);
-            },
-          },
-        },
-        {
-          title,
-          content,
-        }
-      );
-    },
-  });
-  app.mount(container);
+	const app = createApp({
+		render() {
+			return h(
+				Dialog,
+				{
+					closeOnOverlay,
+					visible: true,
+					'onUpdate:visible': (vis: Boolean) => {
+						if (!vis) {
+							close();
+						}
+					},
+					cancel,
+					ok,
+				},
+				{
+					title,
+					content,
+				}
+			);
+		},
+	});
+	app.mount(container);
 };
