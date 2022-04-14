f38d38a7ba5ce3f125b4818f22097f2b36e71a99
DesnLee <jiakun.ui@gmail.com>
2022-04-14 16:33:10 +0800

    替换为局部 yarn 管理，并使用 prettier 格式化

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,46 @@
 import vue from 'rollup-plugin-vue'
-import { terser } from "rollup-plugin-terser"
+import { terser } from 'rollup-plugin-terser'
 import typescript from 'rollup-plugin-typescript2'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import postcss from 'rollup-plugin-postcss'
-import commonjs from 'rollup-plugin-commonjs';
-import postcssImport from 'postcss-import';
+import commonjs from 'rollup-plugin-commonjs'
+import postcssImport from 'postcss-import'
 
 const overrides = {
   compilerOptions: { declaration: true },
-  exclude: ["tests/**/*.ts", "tests/**/*.tsx"]
+  exclude: ['tests/**/*.ts', 'tests/**/*.tsx'],
 }
 
 export default {
   input: 'src/index.ts',
-  output: [{
-    globals: {
-      vue: 'Vue'
+  output: [
+    {
+      globals: {
+        vue: 'Vue',
+      },
+      name: 'desn-ui',
+      file: 'lib/index.js',
+      format: 'umd',
+      plugins: [terser()],
     },
-    name: 'desn-ui',
-    file: './lib/index.js',
-    format: 'umd',
-    plugins: [terser()]
-  }, {
-    name: 'desn-ui',
-    file: './lib/index.module.js',
-    format: 'es',
-    plugins: [terser()]
-  }],
+    {
+      name: 'desn-ui',
+      file: 'lib/index.module.js',
+      format: 'es',
+      plugins: [terser()],
+    },
+  ],
   plugins: [
     nodeResolve(),
     typescript({ tsconfigOverride: overrides }),
     vue(),
     postcss({
-      extensions: [".css"],
+      extensions: ['.css'],
       extract: true,
-      plugins: [postcssImport()]
+      plugins: [postcssImport()],
     }),
     commonjs({
-      include: [
-        "node_modules/**",
-        "node_modules/**/*"
-      ]
+      include: ['node_modules/**', 'node_modules/**/*'],
     }),
     // scss({ include: /\.scss$/, sass: dartSass }),
     // esbuild({
@@ -53,5 +53,5 @@ export default {
     // }),
     // RollupPluginTypescript2(),
   ],
-  external: ['vue', 'echarts', 'lodash-es']
+  external: ['vue', 'echarts', 'lodash-es'],
 }
